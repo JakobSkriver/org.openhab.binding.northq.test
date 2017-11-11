80667e9bab032e32aba0e079c53cffa8b45c85bf
DarkToucan <danmail@mail.dk>
2017-11-11 16:46:11 +0100

    fixed build failure

--- src/test/java/org/openhab/binding/northq/handler/NorthQPlugHandlerTest.java
+++ src/test/java/org/openhab/binding/northq/handler/NorthQPlugHandlerTest.java
@@ -3,7 +3,6 @@ package org.openhab.binding.northq.handler;
 import java.util.ArrayList;
 
 import org.eclipse.smarthome.core.thing.Thing;
-import org.eclipse.smarthome.core.thing.ThingTypeUID;
 import org.eclipse.smarthome.core.thing.binding.ThingHandlerCallback;
 import org.mockito.Mock;
 import org.openhab.binding.northq.internal.model.NorthNetwork;
@@ -18,7 +17,7 @@ public class NorthQPlugHandlerTest {
 
     // private ThingHandler handler;
 
-    private ThingTypeUID thingTypeUID = new ThingTypeUID("northq", "qPlug");
+    // private ThingTypeUID thingTypeUID = new ThingTypeUID("northq", "qPlug");
 
     @Mock
     private ThingHandlerCallback callback;
@@ -32,45 +31,45 @@ public class NorthQPlugHandlerTest {
      * @Before
      * public void setUp() throws Exception {
      * initMocks(this);
-     * 
+     *
      * when(thing.getThingTypeUID()).thenReturn(thingTypeUID);
-     * 
+     *
      * // handler = new NorthQPlugHandler(thing);
      * // handler.setCallback(callback);
-     * 
+     *
      * services = new NorthqServices();
      * credentialsServices = new CredentialsService();
      * user = credentialsServices.getUserCredentials();
      * network = services.mapNorthQNetwork(user.get(0), user.get(1));
-     * 
+     *
      * }
-     * 
+     *
      * @Test
      * public void plugOnHandlerTest() {
      * // Mockito.when(thing.getStatus()).thenReturn(handler.getThing().getStatus());
      * // NorthQConfig config = new NorthQConfig();
-     * 
+     *
      * // Next 2 lines, either here or in setUp()
      * NorthQPlugHandler handler = spy(new NorthQPlugHandler(thing));
      * handler.setCallback(callback);
-     * 
+     *
      * // I don't believe we need the next few lines. They are used for changing variables in NorthQBindingConstants
      * // when(thing.getConfiguration()).thenAnswer(a -> {
      * // Configuration conf = new Configuration();
      * // // conf.put(NorthQBindingConstants.CHANNEL_QPLUG, "channelPlug");
      * // return conf;
      * // });
-     * 
+     *
      * // If the qPlug is connected to the qStick, handler.initialize() should set ThingStatus.ONLINE for qPlug
      * handler.initialize();
-     * 
+     *
      * assertThat(handler.getThing().getStatus(), is(equalTo(ThingStatus.ONLINE)));
      * }
-     * 
+     *
      * @Test
      * public void plugOffHandlerTest() {
      * assertTrue(true);
-     * 
+     *
      * }
      */
 
